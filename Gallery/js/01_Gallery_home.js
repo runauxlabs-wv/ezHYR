aff4927a0459ec79210e96ca97e3ec752de1b557
raraDesign <rara805@naver.com>
2022-05-26 13:57:42 +0900

    mobile menu

--- Gallery/js/01_Gallery_home.js
+++ Gallery/js/01_Gallery_home.js
@@ -1,1 +1,10 @@
-
+$(function () {
+    $("#open").click(function () {
+        $(".menu").toggleClass("on");
+    });
+});
+// $(function () {
+//     $("#close").click(function () {
+//         $(".menu").toggleClass("off");
+//     });
+// });
