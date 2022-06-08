5d43d8efa99f5d06191ae693f71dd4b4847b6a36
raraDesign <rara805@naver.com>
2022-06-08 15:45:39 +0900

    행사소개

--- Star/js/02_Star_행사소개.js
+++ Star/js/02_Star_행사소개.js
@@ -1,11 +1,24 @@
 //tab 스크립트
 $(document).ready(function () {
 
-    $('ul.tabs li').click(function () {
+    $('.night ul.tabs li').click(function () {
         var tab_id = $(this).attr('data-tab');
 
-        $('ul.tabs li').removeClass('current');
-        $('.tab-content').removeClass('current');
+        $('.night ul.tabs li').removeClass('current');
+        $('.night .tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#" + tab_id).addClass('current');
+    })
+
+});
+$(document).ready(function () {
+
+    $('.sun ul.tabs li').click(function () {
+        var tab_id = $(this).attr('data-tab');
+
+        $('.sun ul.tabs li').removeClass('current');
+        $('.sun .tab-content').removeClass('current');
 
         $(this).addClass('current');
         $("#" + tab_id).addClass('current');
