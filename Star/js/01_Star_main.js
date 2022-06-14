a28706c5758321d98d074e2f56fc17e4a3d126e0
raraDesign <rara805@naver.com>
2022-06-14 12:50:12 +0900

    mobile

--- Star/js/01_Star_main.js
+++ Star/js/01_Star_main.js
@@ -1,12 +1,9 @@
-
 // 스팟 클릭시 설명창 생성
 $(function () {
     $(".left").click(function () {
         $(".left").toggleClass("on");
     });
-});
 //tab 스크립트
-$(document).ready(function () {
 
     $('ul.tabs li').click(function () {
         var tab_id = $(this).attr('data-tab');
@@ -18,10 +15,8 @@ $(document).ready(function () {
         $("#" + tab_id).addClass('current');
     })
 
-});
 
 //json 데이터 불러오기
-$(document).ready(function() {
     // $(".map>div").click(function() {
     $.ajax({
         url: "../js/place.json", // 어디로 갈거니? // 갈 때 데이터
@@ -45,10 +40,8 @@ $(document).ready(function() {
             alert('error');
         }
     });
-});
 
 //코스 애니메이션
-$(function(){
     $('#btN1').click(function(){ 
         $('.n_course1').css('display','block');
         $('#n1-1,#n1-2,#n1-3,#n1-5').css('animation-name','Cn1');
@@ -78,39 +71,105 @@ $(function(){
         $('#n5-1,#n5-2,#n5-3,#n5-4,#n5-5').css('animation-name','Cn1-1');
         $('.n_course1,.n_course2,.s_course1,.s_course2').css('display','none');
     });
-  });
-
   
-  //toggle버튼 테마바꾸기
-  $(document).ready(function() {
-    $("input:checkbox").on('click', function() {
-          if ( $(this).prop('checked') ) {
-            $(this).parent().addClass("selected");
-            $("html").css("background", "#f0f0f0");
-            $("header .logo").css({"background":"url(../img/Logo_star_black.svg) no-repeat"});
-            $("footer .cultureLogo").css({"background":"url(../img/Logo_cultural_sun.svg) no-repeat"});
-            $("footer .cultureLogo2").css({"background":"url(../img/Logo_cultural2_sun.svg) no-repeat"});
-            $(".navButton span").css("background", "#40305f");
-            $(".map div p, ul.tabs li").css("color", "#40305f");
-            $(".map div p").css("border-left", "2px solid #40305f");
-            $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
-            $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid #40305f");
-            $(".course_sun, .sun_road, .sun_tree, #sun, .sunT").css("display", "block");
-            $(".star, .course_night, .tree, .lightBar, .road, .ani, #moon, .moonT").css("display", "none");
-          } else {
-            $(this).parent().removeClass("selected");
-            $("html").css("background", "#40305f");
-            $("header .logo").css({"background":"url(../img/Logo_star.svg) no-repeat"});
-            $("footer .cultureLogo").css({"background":"url(../img/Logo_cultural.svg) no-repeat"});
-            $("footer .cultureLogo2").css({"background":"url(../img/Logo_cultural2.svg) no-repeat"});
-            $(".navButton span").css("background", "#FFF4D7");
-            $(".star").css("display", "block");
-            $(".map div p, ul.tabs li").css("color", "white");
-            $(".map div p").css("border-left", "2px solid white");
-            $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
-            $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid white");
-            $(".star, .course_night, .tree, .lightBar, .road, .ani, #moon, .moonT").css("display", "block");
-            $(".course_sun, .sun_road, .sun_tree, #sun, .sunT").css("display", "none");
-          }
-        });
-    });
+//마우스 호버 이벤트
+$(".map div img").hover(function(){
+    $(this).css("transform","translateY(-8px)");
+},function(){
+    $(this).css("transform","translateY(8px)");
+});
+});
+  
+//   //toggle버튼 테마바꾸기
+//   $(document).ready(function() {
+//     $("input:checkbox").on('click', function() {
+//           if ( $(this).prop('checked') ) {
+//             $(this).parent().addClass("selected");
+//             $("html").css("background", "#f0f0f0");
+//             $("header .logo").css({"background":"url(../img/Logo_star_black.svg) no-repeat"});
+//             $(".navButton span").css("background", "#40305f");
+//             $(".map div p, ul.tabs li").css("color", "#40305f");
+//             $(".map div p").css("border-left", "2px solid #40305f");
+//             $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
+//             $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid #40305f");
+//             $(".course_sun, .sun_road, .sun_tree, #sun, .sunT").css("display", "block");
+//             $(".star, .course_night, .tree, .lightBar, .road, .ani, #moon, .moonT").css("display", "none");
+//           } else {
+//             $(this).parent().removeClass("selected");
+//             $("html").css("background", "#40305f");
+//             $("header .logo").css({"background":"url(../img/Logo_star.svg) no-repeat"});
+//             $(".navButton span").css("background", "#FFF4D7");
+//             $(".star").css("display", "block");
+//             $(".map div p, ul.tabs li").css("color", "white");
+//             $(".map div p").css("border-left", "2px solid white");
+//             $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
+//             $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid white");
+//             $(".star, .course_night, .tree, .lightBar, .road, .ani, #moon, .moonT").css("display", "block");
+//             $(".course_sun, .sun_road, .sun_tree, #sun, .sunT").css("display", "none");
+//           }
+//         });
+//     });
+
+    $(window).resize(function(){ 
+        if (window.innerWidth > 760) {  // 다바이스 크기가 480이상일때 
+            $(document).ready(function() {
+                $("input:checkbox").on('click', function() {
+                      if ( $(this).prop('checked') ) {
+                        $(this).parent().addClass("selected");
+                        $("html, header").css("background", "#f0f0f0bd");
+                        $("header .logo").css({"background":"url(../img/Logo_star_black.svg) no-repeat"});
+                        $(".navButton span").css("background", "#40305f");
+                        $(".map div p, ul.tabs li").css("color", "#40305f");
+                        $(".map div p").css("border-left", "2px solid #40305f");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid #40305f");
+                        $(".course_sun, .sun_road, .sun_tree, #sun, .sunT").css("display", "block");
+                        $(".star, .course_night, .tree, .lightBar, .road, .ani, #moon, .moonT").css("display", "none");
+                      } else {
+                        $(this).parent().removeClass("selected");
+                        $("html").css("background", "#40305f");
+                        $("header .logo").css({"background":"url(../img/Logo_star.svg) no-repeat"});
+                        $(".navButton span").css("background", "#FFF4D7");
+                        $(".star").css("display", "block");
+                        $(".map div p, ul.tabs li").css("color", "white");
+                        $(".map div p").css("border-left", "2px solid white");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid white");
+                        $(".star, .course_night, .tree, .lightBar, .road, .ani, #moon, .moonT").css("display", "block");
+                        $(".course_sun, .sun_road, .sun_tree, #sun, .sunT").css("display", "none");
+                      }
+                    });
+                });
+        
+        } else {
+            $(document).ready(function() {
+                $("input:checkbox").on('click', function() {
+                      if ( $(this).prop('checked') ) {
+                        $(this).parent().addClass("selected");
+                        $("html, header").css("background", "#f0f0f0bd");
+                        $("header .logo").css({"background":"url(../img/Logo_star_black.svg) no-repeat"});
+                        $(".navButton span").css("background", "#40305f");
+                        $(".map div p").css("color", "#40305f");
+                        $(".map div p").css("border-left", "2px solid #40305f");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid #40305f");
+                        $(".course_sun, .sun_road, .sun_tree").css("display", "block");
+                        $(".star, .course_night, .tree, .lightBar, .road, .ani").css("display", "none");
+                      } else {
+                        $(this).parent().removeClass("selected");
+                        $("html, header").css("background", "#40305f");
+                        $("header .logo").css({"background":"url(../img/Logo_star.svg) no-repeat"});
+                        $(".navButton span").css("background", "#FFF4D7");
+                        $(".star").css("display", "block");
+                        $(".map div p, ul.tabs li").css("color", "white");
+                        $(".map div p").css("border-left", "2px solid white");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-left", "none");
+                        $(".map .wrap10 p, .map .wrap8 p, .map .wrap4 p, .map .wrap1 p").css("border-right", "1.5px solid white");
+                        $(".star, .course_night, .tree, .lightBar, .road, .ani").css("display", "block");
+                        $(".course_sun, .sun_road, .sun_tree").css("display", "none");
+                      }
+                    });
+                });    
+        }
+        
+        }).resize();
