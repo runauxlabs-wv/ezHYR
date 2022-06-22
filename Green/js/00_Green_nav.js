d9b47e64b38265e263abacc99a0d5cc39f254720
raraDesign <rara805@naver.com>
2022-06-22 12:51:00 +0900

    mobile nav

--- Green/js/00_Green_nav.js
+++ Green/js/00_Green_nav.js
@@ -1,8 +1,21 @@
-$( document ).ready( function() {
-$(".nav").mouseenter(function () {
-    $('.subNav').addClass("on");
+$(document).ready(function () {
+    $(".nav").mouseenter(function () {
+        $('.subNav').addClass("on");
+    });
+    $(".subNav").mouseleave(function () {
+        $('.subNav').removeClass("on");
+    });
+    $(".nav").mouseenter(function () {
+        $('.subNav').addClass("on");
+    });
+    $(".subNav").mouseleave(function () {
+        $('.subNav').removeClass("on");
+    });
 });
-$(".subNav").mouseleave(function () {
-    $('.subNav').removeClass("on");
+
+$("#open").click(function () {
+    $(".nav").addClass("on");
 });
+$("#close").click(function () {
+    $(".nav").removeClass("on");
 });
