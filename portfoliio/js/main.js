ae934ddf0522ac88ff5601441df20be43c4e689f
raraDesign <rara805@naver.com>
2022-07-07 12:44:13 +0900

    main 수정

---
+++ portfoliio/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    var welcomeSection = $('.welcome-section'),
+    enterButton = welcomeSection.find('.enter-button');
+
+    setTimeout(function(){
+      welcomeSection.removeClass('content-hidden');
+    },800);
+    enterButton.on('click',function(e){
+      e.preventDefault();
+      welcomeSection.fadeOut();
+    })
+  })
